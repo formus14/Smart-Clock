de6b6c83a5d8fdb6a565729a2f0ee972191912e7
Omar Hussein <formus14@users.noreply.github.com>
2016-08-08 23:23:33 +0200

    Update Buzzer.ino

--- Milestones sketches/Buzzer/Buzzer/Buzzer.ino
+++ Milestones sketches/Buzzer/Buzzer/Buzzer.ino
@@ -1,22 +1,23 @@
+// Connect your buzz signal pin to digital pin 8
 int buzzer=8;
 int flag=0; 
 void setup()
 {
   pinMode(buzzer,OUTPUT);
 }
+
 void loop()
 {
   int i;
   if ( flag== 0){
-    for(i=0;i<255;i++) //output sound of one frequency
+    for(i=0;i<255;i++) // output sound of one frequency
     {
-      digitalWrite(buzzer,HIGH);//make a sound
+      digitalWrite(buzzer,HIGH);// Buzz sound
       delay(1);//delay 1ms
-      digitalWrite(buzzer,LOW);//silent
+      digitalWrite(buzzer,LOW);// silent
       delay(1);//delay 1ms
     }
     flag=1;
-    digitalWrite(buzzer,LOW);//silent
-
+    digitalWrite(buzzer,LOW);// silent
   }
 }
